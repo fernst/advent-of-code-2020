5c1bdd230548442b185e95d1d63aee19843085e6
Fernando Velasquez <fevelasquez@google.com>
2020-12-05 12:29:47 -0500

    Added day 5.

--- src/com/fernst/aoc/Main.java
+++ src/com/fernst/aoc/Main.java
@@ -4,12 +4,13 @@ import com.fernst.aoc.days.day1.Day1;
 import com.fernst.aoc.days.day2.Day2;
 import com.fernst.aoc.days.day3.Day3;
 import com.fernst.aoc.days.day4.Day4;
+import com.fernst.aoc.days.day5.Day5;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("AOC 2020\n");
         //(new Day0()).run();
-        (new Day4()).run();
+        (new Day5()).run();
     }
 }
---
+++ src/com/fernst/aoc/days/day5/Day5.java
@@ -0,0 +1,99 @@
+package com.fernst.aoc.days.day5;
+
+import com.fernst.aoc.days.Day;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Day5 extends Day {
+  public enum Direction {
+    BOTTOM_LEFT,
+    TOP_RIGHT;
+  }
+
+  public Day5() {
+    inputFile = "/Users/fevelasquez/Development/advent-of-code-2020/src/com/fernst/aoc/days/day5/input0.txt";
+  }
+
+  @Override
+  public void part1() {
+    int maxSeatId = -1;
+    String input = getInput();
+
+    for (String line : input.split("\n")) {
+      int seatId = findSeatId(line);
+      maxSeatId = Math.max(maxSeatId, seatId);
+    }
+
+    System.out.println(String.format("Max seat ID is %d", maxSeatId));
+  }
+
+  @Override
+  public void part2() {
+    List<Integer> seenSeatIds = new ArrayList<>();
+    String input = getInput();
+
+    for (String line : input.split("\n")) {
+      int seatId = findSeatId(line);
+      seenSeatIds.add(seatId);
+    }
+
+    seenSeatIds.sort(Comparator.naturalOrder());
+
+    int seatId = -1;
+
+    for (int i = 0; i < seenSeatIds.size() - 1; i++) {
+      if (seenSeatIds.get(i) + 1 != seenSeatIds.get(i + 1)) {
+        seatId = seenSeatIds.get(i) + 1;
+        break;
+      }
+    }
+
+    System.out.println("My seat ID was " + seatId);
+  }
+
+  public int findSeatId(String line) {
+    String rowString = line.substring(0, 7);
+    String colString = line.substring(7, 10);
+    int row = findRow(rowString);
+    int col = findCol(colString);
+    int seatId = (row * 8) + col;
+
+    return seatId;
+  }
+
+  public int findRow(String row) {
+    int[] range = new int[]{0, 127};
+
+    for (int i = 0; i < row.length(); i++) {
+      char c = row.charAt(i);
+      range = binarySearch(c == 'F' ? Direction.BOTTOM_LEFT : Direction.TOP_RIGHT, range);
+    }
+
+    return range[0];
+  }
+
+  public int findCol(String col) {
+    int[] range = new int[]{0, 7};
+
+    for (int i = 0; i < col.length(); i++) {
+      char c = col.charAt(i);
+      range = binarySearch(c == 'L' ? Direction.BOTTOM_LEFT : Direction.TOP_RIGHT, range);
+    }
+
+    return range[0];
+  }
+
+  public int[] binarySearch(Direction dir, int[] range) {
+    int min = range[0];
+    int max = range[1];
+
+    if (dir == Direction.BOTTOM_LEFT) {
+      return new int[]{min, ((max + min) / 2)};
+    } else {
+      return new int[]{((max + min) / 2) + 1, max};
+    }
+  }
+}
